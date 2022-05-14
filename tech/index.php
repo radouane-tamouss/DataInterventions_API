06f2a4f8a5d83c7919b2ecdc039f08b172cb8c4f
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-14 11:52:07 +0100

    update files

--- tech/index.php
+++ tech/index.php
@@ -1,1 +1,11 @@
-include('../dbConnection.php');
+<?php
+include('dbConnection.php');
+if(isset($_POST['submit']))	{
+	$url = $_POST['url'];				
+	$client = curl_init($url);
+	curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+	$response = curl_exec($client);		
+	$result = json_decode($response);	
+	print_r($result);		
+}
+?>
