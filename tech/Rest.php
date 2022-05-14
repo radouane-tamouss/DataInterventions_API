3faa4eb57b1d2b1c013cb70eac5afe6c207c2451
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-14 11:46:23 +0100

    update files

--- tech/Rest.php
+++ tech/Rest.php
@@ -26,4 +26,25 @@ function insertEmployee($empData){
 	header('Content-Type: application/json');
 	echo json_encode($empResponse);
 }
+
+
+?>
+<?php
+ function getEmployee($empId) {		
+	$sqlQuery = '';
+	if($empId) {
+		$sqlQuery = "WHERE id = '".$empId."'";
+	}	
+	$empQuery = "
+		SELECT empid, empName, empCity, empMobile, empEmail,date_tech, tech_pass 
+		FROM ".$this->technician_tb." $sqlQuery
+		ORDER BY id DESC";	
+	$resultData = mysqli_query($this->dbConnect, $empQuery);
+	$empData = array();
+	while( $empRecord = mysqli_fetch_assoc($resultData) ) {
+		$empData[] = $empRecord;
+	}
+	header('Content-Type: application/json');
+	echo json_encode($empData);	
+}
 ?>
