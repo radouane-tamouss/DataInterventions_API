1859b0e67f2fcd549f823c7f001fb5a03599633f
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-16 11:53:37 +0100

    update files

--- getInterventions.php
+++ getInterventions.php
@@ -1,11 +1,24 @@
 <?php
 $servername="localhost";
-$username ="root"
+$username ="root";
 $password="";
 $dbname="api";
 // Create connection
 $conn=new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error){
-   die("Connection failed:"$conn->connect_error); 
-$sql "SELECT * from assignwork_tb WHERE assign_tech= 'Kabir'"; 
+   die("Connection failed:" .$conn->connect_error); 
+}
+$sql= "SELECT * from assignwork_tb WHERE assign_tech= 'Kabir'"; 
+
+$result = $conn->query($sql);
+
+$array = array();
+
+while($raw = $result->fetch_assoc()){
+
+    array_push($array, $raw);
+}
+echo json_encode($array);
+
+?>
