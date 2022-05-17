1429fb36cf5f56a44ec014eb87083be255232bd5
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-17 16:08:09 +0100

    fix data not showing on db

--- insertInterventions.php
+++ insertInterventions.php
@@ -11,30 +11,32 @@ if ($conn->connect_error){
    die("Connection failed:" .$conn->connect_error); 
 }
 
-$task = $_GET['task'];
+// $task = $_GET['task'];
 
-$sql= "INSERT INTO tasks(task) values('$task')";
-// $request_info = $_GET('request_info');
-// $request_id = $_GET('request_id');
-// $request_desc = $_GET('request_desc');
-// $requester_name = $_GET('requester_name');
-// $requester_add1 = $_GET('requester_add1');
-// $requester_add2 = $_GET('requester_add2');
-// $requester_city = $_GET('requester_city');
-// $requester_state = $_GET('requester_state');
-// $requester_zip = $_GET('requester_zip');
-// $requester_email = $_GET('requester_email');
-// $requester_mobile = $_GET('requester_mobile');
-// $assign_tech = $_GET('assign_tech');
-// $assign_date = $_GET('assign_date');
+// $sql= "INSERT INTO tasks(task) values('$task')";
 
-// $sql = "INSERT INTO assignwork_tb(request_id,request_info,request_desc, requester_name , 
-//          requester_add1 , requester_add2 , requester_city , requester_state, requester_zip,
-//           requester_email, requester_mobile,assign_tech,assign_date) VALUES($request_id,$request_info,
-//           $request_desc,
-//            $requester_name,$requester_add1,$requester_add2,
-//             $requester_city, $requester_state, $requester_zip, $requester_email,
-//             $requester_mobile,$assign_tech,$assign_date) ";
+
+$request_info = $_GET['request_info'];
+$request_id = $_GET['request_id'];
+$request_desc = $_GET['request_desc'];
+$requester_name = $_GET['requester_name'];
+$requester_add1 = $_GET['requester_add1'];
+$requester_add2 = $_GET['requester_add2'];
+$requester_city = $_GET['requester_city'];
+$requester_state = $_GET['requester_state'];
+$requester_zip = $_GET['requester_zip'];
+$requester_email = $_GET['requester_email'];
+$requester_mobile = $_GET['requester_mobile'];
+$assign_tech = $_GET['assign_tech'];
+$assign_date = $_GET['assign_date'];
+
+$sql = "INSERT INTO assignwork_tb(request_id,request_info,request_desc, requester_name , 
+         requester_add1 , requester_add2 , requester_city , requester_state, requester_zip,
+          requester_email, requester_mobile,assign_tech,assign_date) VALUES('$request_id','$request_info',
+          '$request_desc',
+           '$requester_name','$requester_add1','$requester_add2',
+            '$requester_city', '$requester_state', '$requester_zip', '$requester_email',
+            '$requester_mobile','$assign_tech','$assign_date') ";
 
 
 $result = $conn->query($sql);
