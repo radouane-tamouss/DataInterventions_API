048e95db652c28e745d05356b4fda9953d4785fa
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-17 15:01:29 +0100

    add insertIntervention.php

--- insertInterventions.php
+++ insertInterventions.php
@@ -3,23 +3,11 @@ $servername="localhost";
 $username ="root";
 $password="";
 $dbname="api";
+
 // Create connection
 $conn=new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error){
    die("Connection failed:" .$conn->connect_error); 
 }
-$tech_name = 'Tech1';
-$sql= "SELECT * from assignwork_tb WHERE assign_tech= '$tech_name'"; 
 
-$result = $conn->query($sql);
-
-$array = array();
-
-while($raw = $result->fetch_assoc()){
-
-    array_push($array, $raw);
-}
-echo json_encode($array);
-
-?>
