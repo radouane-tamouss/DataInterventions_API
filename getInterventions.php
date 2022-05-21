cb8e3bc0fa9d9724f778e68f7bcc579cf0326177
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-21 09:29:19 +0100

    :card_file_box: database related changes

--- getInterventions.php
+++ getInterventions.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username ="root";
 $password="";
-$dbname="inter";
+$dbname="api";
 // Create connection
 $conn=new mysqli($servername, $username, $password, $dbname);
 // Check connection
